6d0da57ea5eab3b7b333ff9b805a8078ca535092
Ethan Ambabo <eambabo@gmail.com>
2025-12-30 15:58:44 -0500

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Ethan Ambabo on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
